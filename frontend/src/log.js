7d54799f1b2f372d631ee599c2ec5375d42fe6aa
raaksss <rakshitamanocha@gmail.com>
2023-10-29 18:40:35 +0530

    Fixed broken links

--- frontend/src/log.js
+++ frontend/src/log.js
@@ -1,10 +1,10 @@
 document.getElementById("login-button").addEventListener("click", function() {
-    var client_id = "84fad8cd66834378b3336e18558015cf"; // Your client id
-    var redirect_uri = encodeURIComponent("http://127.0.0.1:5500/welcome.html"); // Your desired redirect URI
+    var client_id = "84fad8cd66834378b3336e18558015cf"; 
+    var redirect_uri = encodeURIComponent("http://127.0.0.1:5500/frontend/src/welcome.html");
     var url = "https://accounts.spotify.com/authorize";
     url += "?response_type=token";
     url += "&client_id=" + encodeURIComponent(client_id);
-    url += "&redirect_uri=" + redirect_uri; // Include the redirect URI here
+    url += "&redirect_uri=" + redirect_uri;
     window.location = url;
     
 }, false);
